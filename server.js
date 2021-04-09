c421c3261997cd31ff3845656cc8bb58478d8075
za-01 <zaeemansari2001@gmail.com>
2021-04-09 20:50:49 +0530

    Minor changes to layout

--- server.js
+++ server.js
@@ -24,8 +24,25 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(path.resolve(__dirname, "public")));
-
+app.get('/', function (req, res) {
+	var dishes=[
+		{
+			dishID:"0",dishName:"Pizza", price:"200", typeOfFood:"Fastfood", Image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsh-vwIw4nZLQ5lUJf-hnz7uQiEykeKqMfeg&usqp=CAU"
+		},
+		{
+			dishID:"1",dishName:"Noodles", price:"70", typeOfFood:"Fastfood", Image:"https://recipetineats.com/wp-content/uploads/2019/11/Lo-Mein_5.jpg"
+		},
+		{
+			dishID:"2",dishName:"Samosa", price:"30", typeOfFood:"Fastfood", Image:"https:rakskitchen.net/wp-content/uploads/2015/12/samosa-recipe-500x375.jpg"
+		},
+		{
+			dishID:"3",dishName:"Paneer Kofta", price:"150", typeOfFood:"Main Course", Image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVl_HTICim5UbPVrGB3lw0Ayod39rLeFaBag&usqp=CAU"
+		},
+	]
+	res.render("menu", {dishes:dishes});
+})
 // ROUTES
+
 app.use("/employees", employeeRoutes);
 app.use("/dishes", dishRoutes);
 
