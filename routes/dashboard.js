1c20eb51faaed576cc37fc1a4b66e32bcfe5b408
za-01 <zaeemansari2001@gmail.com>
2021-04-10 15:26:53 +0530

    added tables page to /dashboard/tables

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const dashboardcontroller = require("../contollers/dashboardcontroller");
 
 router.get("/", dashboardcontroller.dashboard);
+router.get("/tables", dashboardcontroller.tables);
 
 module.exports = router;
 	
