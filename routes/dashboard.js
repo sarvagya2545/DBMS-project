dba365ca3104ee0339185e45b30e59a52719bcb9
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-04-11 15:24:55 +0530

    dashboard fix

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const dashboardcontroller = require("../contollers/dashboardcontroller");
 
 router.get("/", dashboardcontroller.dashboard);
-router.get("/tables", dashboardcontroller.tables);
-
+router.get("/table", dashboardcontroller.tables);
 
 module.exports = router;
-	
