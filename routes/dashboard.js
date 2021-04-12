bf1c9f72f7474b9cab5894c2b11acc5a8692d398
Akshat-512 <singhaiakshat2001@gmail.com>
2021-04-12 16:50:55 +0530

    commit

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 const dashboardcontroller = require("../contollers/dashboardcontroller");
 
+
 router.get("/", dashboardcontroller.dashboard);
 router.get("/tables", dashboardcontroller.tables);
+router.get("/customers", dashboardcontroller.customers);
+router.get("/menu", dashboardcontroller.menu);
+router.get("/profile",dashboardcontroller.profile);
+router.get("/orders",dashboardcontroller.orders);
+router.get("/staffmanagement",dashboardcontroller.staffmanagement);
 
 
 module.exports = router;
