3a2d4c340fdcb56afdf1f198378c5f5f97222f07
za-01 <zaeemansari2001@gmail.com>
2021-04-12 17:47:03 +0530

    Added Staff Managemet, removed menu, removed animation

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,10 +7,9 @@ router.get("/", dashboardcontroller.dashboard);
 
 router.get("/tables", dashboardcontroller.tables);
 router.get("/customers", dashboardcontroller.customers);
-router.get("/menu", dashboardcontroller.menu);
 router.get("/profile",dashboardcontroller.profile);
 router.get("/orders",dashboardcontroller.orders);
-router.get("/staffmanagement",dashboardcontroller.staffmanagement);
+router.get("/staff_management",dashboardcontroller.staff_management);
 
 
 module.exports = router;
