0c744303df33ab7b5ae10831b87b6350f34b234e
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-04-18 19:53:42 +0530

    dish controller 2

--- routes/dishes.js
+++ routes/dishes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const DishController = require('../contollers/dishController');
+const { ensureAuthenticated } = require("../middleware/auth");
 
 router.get("/get", function (req, res) {
 	res.send("dishes");
 });
 
-router.post('/add-order', DishController.addOrder)
+router.post('/add-order', ensureAuthenticated, DishController.addOrder)
 
 module.exports = router;
