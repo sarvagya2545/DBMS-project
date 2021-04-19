a77b2470c2126b87f652d2390de01e0aa9dae82c
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-04-19 19:39:13 +0530

    logout and create header partial

--- routes/employee.js
+++ routes/employee.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const EmployeeController = require("../contollers/employee");
+const { ensureAuthenticated } = require('../middleware/auth');
 
 router.get("/", EmployeeController.getEmployees);
 router.get("/login", EmployeeController.loginPage);
 router.post("/login", EmployeeController.login);
 router.get("/new", EmployeeController.newEmployeePage);
 router.post("/new", EmployeeController.newEmployee);
+router.get('/logout', (req, res) => {
+    console.log('Something');
+    req.session.destroy();
+    res.redirect('/');
+});
 
 module.exports = router;
